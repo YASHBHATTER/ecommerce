b18207e30815ec7c62eac37d2a653c800edaafe5
YASHBHATTER <yb8173@srmist.edu.in>
2022-03-01 23:27:48 +0530

    Made Models and rendered product dynamically with ImageURL

--- store/views.py
+++ store/views.py
@@ -1,9 +1,11 @@
 from multiprocessing import context
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request,'store/store.html',context)
 
 def cart(request):
